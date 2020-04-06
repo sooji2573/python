9249762e14dcb2b47de643f4761dfec4ef5cd36b
sooji2573 <sooji2573@naver.com>
2020-04-06 11:47:17 +0900

    0406 function, module

---
+++ src/function.py
@@ -0,0 +1,16 @@
+
+def add(num1, num2):
+    return num1 + num2
+
+print(add(1, 2))
+
+
+def add_mul(num1, num2):
+    return num1+num2, num1*num2
+
+print(add_mul(1, 2))
+
+my_add, my_mul = add_mul(1, 2)
+
+print(my_add)
+print(my_mul)
---
+++ src/module.py
@@ -0,0 +1,11 @@
+import random
+
+students = ['1', '2', '3', '4', '5', '6']
+
+print(random.choice(students))
+
+print(random.sample(students, 2))
+
+print(random.sample(range(1, 46), 6))
+
+print(random.randint(8, 10))
